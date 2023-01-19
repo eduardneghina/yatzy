b19981a7a7067785e759980f7515b3c02dddf6cd
eduardneghina <41010274+eduardneghina@users.noreply.github.com>
2023-01-19 13:05:03 +0200

    a more clear class and function

--- app/src/main/java/com/example/yatzy/Calculations.java
+++ app/src/main/java/com/example/yatzy/Calculations.java
@@ -1,20 +1,14 @@
+package com.example.yatzy;
+
 import java.util.Random;
 
 public class Calculations {
 
-}
-
-class Dice {
-
+    // function to generate a random number from 1 to 6
     public static int getRandomNumberForDice() {
         final int min = 1;
         final int max = 6;
         final int random = new Random().nextInt((max - min) + 1) + min;
         return random;
     }
-
-    int diceRandomNumber = getRandomNumberForDice();
 }
-
-
-//comment
